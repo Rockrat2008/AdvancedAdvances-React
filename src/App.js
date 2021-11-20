04bd76f83fcec961d02e9e6c1aebd47fa30a3f02
Michael OBrien <rockrat2008@gmail.com>
2021-11-20 12:39:10 -0800

    Updated CSS imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Main from "./components/MainComponent";
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends Component {
   render() {
